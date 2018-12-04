3377753e2f6687e582f41eb0eba2b9b6d02cbef8
Florence Hapgood <flohapgood@icloud.com>
2018-12-04 17:41:21 +0000

    Simple vanilla javascript button
    User can click on Cristian and see nice stems, wrirten in vanilla javascript for practise

--- interface.js
+++ interface.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-console.log("whatup")
-})
+// $(document).ready(function() {
+// console.log("whatup")
+// })
 
 $('#Dion').click(function(){
   $('#DionQuote').text("He does like to shop Cher. And the man can dress.")
 })
+
+document.getElementById("Cristian").addEventListener("click", function() { document.getElementById("CristianQuote").innerHTML = "Nice Stems" })
