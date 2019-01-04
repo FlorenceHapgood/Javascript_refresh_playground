f9ec04ee5bedf0b1a49c05ace34c9c4774e90b5a
Florence Hapgood <flohapgood@icloud.com>
2019-01-04 19:28:52 +0000

    Cypress added and some sample tests

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,19 @@
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(true)
+  })
+})
+
+describe('My First Test', function() {
+  it('Visits the Kitchen Sink', function() {
+    cy.visit('https://example.cypress.io')
+  })
+})
+
+describe('My First Test', function() {
+  it('finds the content "type"', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type')
+  })
+})
